2fd50a09a785aff2a74323855ad397f941ce314d
Fabio De Lorenzo <fabio@calisoft.dev>
2020-07-03 16:18:01 -0700

    splash screen

---
+++ app/src/main/java/com/fabio/weatherapp/view/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.fabio.weatherapp.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    startActivity(Intent(this, MainActivity::class.java).apply {
+      addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+    })
+    finish()
+  }
+}
